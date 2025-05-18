ccb9e682a8cf08076fd3de0a940e95ec2709db6e
JBen3560 <joey.benich@gmail.com>
2025-05-18 18:31:33 -0400

    Working but slow

--- solver.cpp
+++ solver.cpp
@@ -2,16 +2,24 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cmath>
+#include <chrono>
+#include <unordered_set>
+#include <algorithm>
+#include <unordered_map>
+
+#include <windows.h> // Required for Sleep
+
 using namespace std;
 
 const string FILENAME = "wordlist.txt";
-typedef vector<vector<string>> wordlist;
+typedef unordered_set<string> wordlist;
 typedef vector<vector<pair<char,char>>> grid;
 
 // Prints the puzzle with colors for each different word
-void printPuzzle(const grid& puzzle){
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 6; j++){
+void printPuzzle(const grid& puzzle, int h, int w){
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
             switch(puzzle[i][j].second){
                 case '1':
                     cout << "\033[38;5;1m";
@@ -47,66 +55,125 @@ void printPuzzle(const grid& puzzle){
         }
         cout << endl;
     }
-    cout << endl;
+    cout << "\033[0m" << endl;
 }
 
-// Prints solutions to the puzzle
-void getSolution(int x, int y, int color, string word, const wordlist& words, grid puzzle){
-    
+// Make sure you don't set a color not adjacent to another color
+bool adjacentToColor(const grid& puzzle, int y, int x, char c, int h, int w){
+    // Would return true if this is first time that color is used
+    bool firstOfColor = true;
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            if(puzzle[i][j].second == c){
+                // Check if the color is already on the board
+                firstOfColor = false;
+                // Check if adjacent (and not just x,y)
+                if(((abs(i-y) <= 1) && (abs(j-x) <= 1)) && (i != y || j != x))
+                    return true;
+            }
+        }
+    }
+    return firstOfColor;
+}
+
+// Check whether the sorted letters of each group can be found in a sorted dict
+// (not perfect, but letter groups are likely to be words and it's fast)
+bool validSolution(const wordlist& words, const grid& puzzle, int h, int w){
+    // Build color groups
+    unordered_map<char, string> colorGroups;
+    for(int y = 0; y < h; ++y){
+        for(int x = 0; x < w; ++x){
+            char letter = puzzle[y][x].first;
+            char color = puzzle[y][x].second;
+            colorGroups[color] += letter;
+        }
+    }
+
+    // Check if sorted group letters match any word in the dictionary
+    for(const auto& [color, group] : colorGroups){
+        string sortedGroup = group;
+        sort(sortedGroup.begin(), sortedGroup.end());
+        if(words.find(sortedGroup) == words.end()) return false;
+    }
+    return true;
+}
+
+// Prints solution to the puzzle
+bool getSolution(const wordlist& words, grid& puzzle, int h, int w){
+    // Try grid until the colors are all in touching groups
+    for(int y = 0; y < h; y++){
+        for(int x = 0; x < w; x++){
+            if(puzzle[y][x].second == '0'){
+                for(char c = '1'; c < '9'; c++){
+                    if(adjacentToColor(puzzle,y,x,c,h,w)){
+                        puzzle[y][x].second = c;
+                        if(getSolution(words,puzzle,h,w)) return true;
+                        puzzle[y][x].second = '0';
+                    }
+                }
+                return false;
+            }
+        }
+    }
+
+    // Check whether each group probably contains a word
+    if(validSolution(words,puzzle,h,w)){
+        cout << "\nSolution\n";
+        printPuzzle(puzzle,h,w);
+        return true;
+    } else return false;
 }
 
 int main(){
-    // Get the puzzle
+    // Get the dimensions
     grid puzzle;
+    int w, h;
+    char x;
+    cout << "Enter dimensions (WxH): ";
+    cin >> w >> x >> h;
+    cin.clear();
+    cin.ignore(100,'\n');
+
+    // Get the letters
     cout << "Enter each line of the puzzle with no spaces." << endl;
-    for(int i = 0; i < 8; i++){
+    for(int i = 0; i < h; i++){
         vector<pair<char,char>> row;
         string buffer;
         cout << "Line " << i+1 << ": ";
         getline(cin, buffer);
-        for(int j = 0; j < 6; j++){
+        for(int j = 0; j < w; j++){
             pair<char,char> item = {buffer[j],'0'};
             row.push_back(item);
         }
         puzzle.push_back(row);
     }
     
-    // Read in valid words to their respective length list
+    // Read in valid words
     ifstream file(FILENAME);
     wordlist words;
-    for(int i = 0; i < 7; i++){
-        // Lists for 3, 4, 5, 6, 7, 8, and 9+ letter words
-        vector<string> list;
-        words.push_back(list);
-    }
-
-    // Read in file
     string word;
     if(!file){
         cout << "Error opening word list" << endl;
         return 1;
     }
     while (file >> word){
-        if(word.length() >= 3){
-            if(word.length() < 9) words[word.length()-3].push_back(word);
-            else words[6].push_back(word);
+        if(word.length() >= 4){
+            sort(word.begin(),word.end());
+            words.insert(word);
         }
     }
     file.close();
 
-    // Solve the puzzle
-    string word = "";
-    getSolution(0, 0, 1, word, words, puzzle);
-    cout << "\n--- Solution ---\n";
-    printPuzzle(puzzle);
+    // Solve the puzzle and see how long it took
+    auto start = chrono::high_resolution_clock::now();
+    if(!getSolution(words, puzzle, h, w))
+        cout << "No solution found." << endl;
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+    cout << "Execution time: " << duration.count() << " s" << endl;
 
-    /* // Print loaded words (optional)
-    for(int i = 0; i < 7; i++){
-        for(int j = 0; j < 10; j++){
-            cout << words[i][j] << endl;
-        }
-        cout << endl;
-    } */
+    /* // Print loaded words
+    for(auto it = words.begin(); it != words.end(); it++) cout << *it << endl; */
 
     return 0;
 }
